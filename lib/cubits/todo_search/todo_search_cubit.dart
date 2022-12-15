5159cd28a9573d624f0c9d34942a12e27cc28047
Vitaly Shastun <kobbleton@gmail.com>
2022-12-15 14:28:54 +0530

    Create ToDo Search Cubit
    Create ToDo Search Cubit

---
+++ lib/cubits/todo_search/todo_search_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+
+import 'todo_search_state.dart';
+
+class TodoSearchCubit extends Cubit<TodoSearchState> {
+  TodoSearchCubit() : super(TodoSearchState.initial());
+
+  void setSearchTerm(String newSearchTerm) {
+    emit(state.copyWith(searchTerm: newSearchTerm));
+  }
+}
