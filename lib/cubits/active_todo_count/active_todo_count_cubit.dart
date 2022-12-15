135c868f5a082a8e01188d2ea50429bdb2aabdc1
Vitaly Shastun <kobbleton@gmail.com>
2022-12-15 15:29:58 +0530

    Create ToDo Count Cubit
    Create ToDo Count Cubit

--- lib/cubits/active_todo_count/active_todo_count_cubit.dart
+++ lib/cubits/active_todo_count/active_todo_count_cubit.dart
@@ -1,8 +1,33 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+// ignore_for_file: depend_on_referenced_packages
 
-part 'active_todo_count_state.dart';
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:todo_app/cubits/todo_list/todo_list_cubit.dart';
+import '../../models/todo_model.dart';
+import '../todo_list/todo_list_state.dart';
+import 'active_todo_count_state.dart';
 
 class ActiveTodoCountCubit extends Cubit<ActiveTodoCountState> {
-  ActiveTodoCountCubit() : super(ActiveTodoCountInitial());
+  late final StreamSubscription todoListSubscription;
+  final TodoListCubit todoListCubit;
+  ActiveTodoCountCubit({
+    required this.todoListCubit,
+  }) : super(ActiveTodoCountState.initial()) {
+    todoListSubscription =
+        todoListCubit.stream.listen((TodoListState todoListState) {
+      print('todoListState: $todoListState');
+
+      final int currentActiveTodoCount = todoListState.todoList
+          .where((Todo todo) => !todo.isCompleted)
+          .toList()
+          .length;
+
+      emit(state.copyWith(activeTodoCount: currentActiveTodoCount));
+    });
+  }
+  @override
+  Future<void> close() {
+    todoListSubscription.cancel();
+    return super.close();
+  }
 }
